ba6d67f2cdf54e9ceddf45256d043875724ec77c
Aditya Raj <aditya0503sfs@gmail.com>
2025-02-11 17:28:20 +0530

    add js code to github

---
+++ week-04/2-js conditionals and loops/outfit.js
@@ -0,0 +1,14 @@
+// server call, rainy, cloudy, sunny
+
+let weather = "cloudy";
+
+if (weather === "rainy") {
+  console.log("it's rainly, unbrella le jaana bhai!");
+} else if (weather === "cloudy") {
+  console.log("it's cloudy, jacket le jaana bhai!");
+} else {
+  console.log("it's sunny, maaze kro bhai!");
+}
+
+
+console.log(weather === "rainy");
