772ae7ed16dc545bf3a25e133cdaf0d3f9f8f72b
Aditya Raj <aditya0503sfs@gmail.com>
2025-02-11 22:51:20 +0530

    add js code to github

---
+++ week-04/2-js conditionals and loops/array.js
@@ -0,0 +1,19 @@
+let fruits = ["apple", "mango", "cherry"];
+let intFruits = ["Kiwi", "Avacado", "Dragon Fruit"];
+
+// console.log(fruits);
+// console.log(intFruits);
+
+// console.log(typeof fruits);
+// console.log(typeof intFruits);
+
+fruits[0] = "blueberry";
+
+console.log(fruits[0]);
+console.log(fruits.length);
+
+fruits.push("banana");
+fruits.unshift("grapes");
+fruits.pop();
+
+console.log(fruits);
