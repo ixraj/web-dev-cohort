30ce0832b6f19c8446e2e8fecf3134fc866f9b6e
Aditya Raj <aditya0503sfs@gmail.com>
2025-02-11 11:43:35 +0530

    add js code to github

---
+++ week-04/2-js conditionals and loops/bootcampProfile.js
@@ -0,0 +1,31 @@
+// String - "". '', ``
+let name = "aditya";
+
+// Number - 20(int), 20.3(float)
+let age = 19;
+
+// Boolean - True, False
+let isPaid = true;
+// let isPaid = false;
+
+// Null
+let favouriteClass = null;
+
+// undefined - bad mein dekhenge
+let homeTown;
+
+// array
+// in array start form 0,1,2,3..
+let skills = [true, false, "html", "css", "js", 45];
+
+// {} - k andar object - key value pair
+let studentProfile = {
+  name: "aditya",
+  age: 19,
+  isPaid: true,
+  skills: ["html", "css", "js"],
+  favouriteClass: null,
+};
+
+// typeof - you can check what type of data
+console.log(typeof homeTown);
